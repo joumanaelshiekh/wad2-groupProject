452b788d7c26bd99e80cf1e48aaa14bb622292c8
KierWats <46569382+KierWats@users.noreply.github.com>
2020-03-18 15:02:09 +0000

    Update urls.py

--- ptfinder/ptfinderApp/urls.py
+++ ptfinder/ptfinderApp/urls.py
@@ -13,12 +13,12 @@ urlpatterns = [
     path('trainer/', views.trainer , name='trainer'),
     path('trainer/<str:t_username>/', views.trainer_profile , name='trainer_profile'), 
     path('book/', views.book , name='book'),
-    path('book/create-booking/', views.create_booking, name='create_booking'),
-    path('book/<str:username/', views.view_bookings , name='view_bookings'),
-    path('book/<str:t_username/', views.view_bookings , name='view_bookings'),
+    path('book/create_booking/', views.create_booking, name='create_booking'),
+    path('book/<str:username>/', views.view_bookings , name='view_bookings'),
+    path('book/<str:t_username>/', views.view_bookings , name='view_bookings'),
     path('contact_us/', views.contact_us , name='contact_us'),
     path('gym/', views.gym , name='gym'), 
-    path('gym/<int:g_id', views.gym_profile , name='gym_profile'), 
+    path('gym/<int:g_id>', views.gym_profile , name='gym_profile'), 
     path('logout/', views.logout , name='logout'),
     #path('', views. , name=''),   #template for easy additions
 ]
